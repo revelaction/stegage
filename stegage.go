7c69e1229151982924d4d94e1ddbb498e5a53350
revelaction <96388231+revelaction@users.noreply.github.com>
2021-12-28 13:35:04 +0100

    make constant no public

--- stegage.go
+++ stegage.go
@@ -19,9 +19,9 @@ import (
 	"github.com/auyer/steganography"
 )
 
-// AGE_HEADER_FIRST_14 contains the first 14 bytes of a valid encrypted age
+// age_header_first_14 contains the first 14 bytes of a valid encrypted age
 // file
-const AGE_HEADER_FIRST_14 = "age-encryption"
+const age_header_first_14 = "age-encryption"
 
 // Encode encrypts data to the age password recipient and after that encodes
 // with LSB steganography the encrypted payload in a copy of image.
@@ -126,7 +126,7 @@ func Decode(password, image io.Reader, out io.Writer) error {
 }
 
 func isPayloadAge(payload []byte) bool {
-	if string(payload[:14]) != AGE_HEADER_FIRST_14 {
+	if string(payload[:14]) != age_header_first_14 {
 		return false
 	}
 
